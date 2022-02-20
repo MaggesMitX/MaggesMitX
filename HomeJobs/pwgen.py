479e247d215f0debd2d0c6b2013d967333a6d83f
00Shinigami <mail@max-ramacher.de>
2022-02-20 15:55:42 +0100

    errorFix

--- HomeJobs/pwgen.py
+++ HomeJobs/pwgen.py
@@ -7,7 +7,7 @@ verbindung = sqlite3.connect("PWdaten.db")
 zeiger = verbindung.cursor()
 
 sql_anweisung = """
-CREATE TABLE Passwörter (
+CREATE TABLEIF NOT EXISTS Passwörter (
 Account VARCHAR(20), 
 Passwort VARCHAR(30), 
 Erstelldatum DATE
@@ -24,4 +24,3 @@ verbindung.close()
 def create_password(pw):
     password = secrets.token_urlsafe(32)
     print("The Password is", password)
-    
