eed4e94490fbe1b144301154bc898e820f5ba9d4
00Shinigami <mail@max-ramacher.de>
2022-02-21 13:29:19 +0100

    update

--- HomeJobs/pwgen.py
+++ HomeJobs/pwgen.py
@@ -3,17 +3,25 @@
 # coding: utf-8
 from datetime import date
 from optparse import Values
+from ssl import _PasswordType
 from tkinter import *
+from sqlite3 import Error
+import time
 import secrets
 import sqlite3
+from tkinter.tix import InputOnly
 
 verbindung = sqlite3.connect("PWdaten.db")
 zeiger = verbindung.cursor()
 today = date.today()
+master = Tk()
+master.geometry("400x180") #Anweisung größe des Bildes
 
 # dd/mm/YY
 act_day = today.strftime("%d/%m/%Y")
 #print("actual day =", act_day)
+
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 sql_anweisung = """
@@ -24,34 +32,38 @@ Erstelldatum DATE
 );"""
 zeiger.execute(sql_anweisung)
 #akutelle Werte der DB committen(Übertragen)
-verbindung.commit()
-#verbindung zur DB schließen
-verbindung.close()
+#verbindung.commit()
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-
+def sql_connection(): #Verbinden mit der Datenbank 
+    try:
+        con = sqlite3.connect(':memory:')
+        time.sleep(3)
+        print("Connection is established: Database is created in memory")
+    except Error:
+        print(Error)
+    finally:
+        con.close()
+sql_connection()
+#Zeige die aktuellen Einträge an!!!! Noch ab ändern  in SELECT statt der function 
 def show_entry_fields():
-   print("Account: %s\nPasswort: %s" % (Account.get(), Passwort.get()))
-
+   print("Account: %s\nPasswort: %s" % (Account, Passwort))
+show_entry_fields()
+#password generieren
+def create_password(pw):
+    crpassword = secrets.token_urlsafe(30)
+    time.sleep(2)
+    print("The Password is", crpassword)
+#Eintrag hinzufügen
 def add_entry():
-    password = secrets.token_urlsafe(32)
-    print("The Password is", password)
-    Account = Account.get()
-    Passwort = Passwort.get()
-    zeiger.execute("SELECT COUNT(*) from Passwörter WHERE Account = ' "+ Account +"' ")
-    zeiger.execute("SELECT COUNT(*) from Passwörter WHERE Passwort = ' "+ password +"' ")
+    zeiger.execute()
     result = zeiger.fetchall
-
-#checking ob der nutzer schon existiert
+#checke ob der nutzer schon existiert
     if int(result[0]) > 0:
         error["text"] = "Error: Account wurde bereits angelegt"
     else:
         error["text"] = "Account mit Passwort angelegt" 
-        zeiger.execute("INSERT INTO Passwörter(Account, Passwort)VALUES(?,?)", (Account, password))
+        zeiger.execute("INSERT INTO Passwörter(Account, Passwort)VALUES(?,?)", (Account, crpassword))
         verbindung.commit()  
-
-master = Tk()
-master.geometry("400x180")
-
 #Error Erzeugen
 error = Message(text="", width=160)
 error.place(x = 30, y = 10)
@@ -66,13 +78,7 @@ Passwort = Entry(master)
 Account.grid(row=0, column=1)
 Passwort.grid(row=1, column=1)
 
-Button(master, text='Speichern', command= add_entry).grid(row=3, column=0, sticky=W, pady=4) #quit durch Speichern ersetzen
+Button(master, text='Speichern', command= add_entry).grid(row=3, column=0, sticky=W, pady=4) 
 Button(master, text='Beenden', command=master.quit).grid(row=3, column=1, sticky=W, pady=4) 
 Button(master, text='Anzeigen', command=show_entry_fields).grid(row=3, column=2, sticky=W, pady=4)
-
-mainloop( )
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-#password generieren
-#def create_password(pw):
-#    password = secrets.token_urlsafe(32)
-#    print("The Password is", password)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - # 
