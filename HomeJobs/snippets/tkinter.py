beaf1df6ea89ec40defaaaf18688387de441bcb1
00Shinigami <mail@max-ramacher.de>
2022-02-28 14:57:53 +0100

    update

---
+++ HomeJobs/snippets/tkinter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#Help:https://www.pythontutorial.net/tkinter/
+
+from tkinter import Tk, BOTH
+from tkinter.ttk import Frame
+
+
+win =Tk()
+
+win.mainloop()
