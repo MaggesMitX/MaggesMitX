03a85d11e582790431fd090883545f459813e54d
00Shinigami <mail@max-ramacher.de>
2022-03-02 09:28:36 +0100

    finish datetime.py

--- HomeJobs/finished.snippets/DateTimestruct.py
+++ HomeJobs/finished.snippets/DateTimestruct.py
@@ -2,12 +2,22 @@
 # coding: utf-8
 
 from datetime import datetime, time
+from dis import findlinestarts
+from re import A
+from time import sleep
+from turtle import goto
 
 # get rid of exact seconds and microseconds
 current_time = datetime.now().time().replace(second=0, microsecond=0)
 
-if current_time == time(15) or current_time == time(18, 30):
-    print("Test")
-    # do stuff
-else:
-    print("error")
+while True:
+    A
+    if current_time == time(15) or current_time == time(10,30 ):
+            print("Job wird gestartet")  
+            # do stuff
+            sleep(60)
+    else:
+            print("error")
+            sleep(60)
+            goto :A
+KeyboardInterrupt
