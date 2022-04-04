878b60c7dbeecd25de78c8ee062c2f7744d25b7f
00Shinigami <mail@max-ramacher.de>
2022-04-04 13:27:23 +0200

    Test

--- PatProjekt/led_animation.py
+++ PatProjekt/led_animation.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python
 import time, math
 from rpi_ws281x import PixelStrip, Color
 import argparse
 
 # LED strip configuration:
-LED_COUNT = 200       # Number of LED pixels.
+LED_COUNT = 515       # Number of LED pixels. 515 Sind es Zeit auf 900s setzen
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 120  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -31,23 +32,29 @@ class RGBFunctions():
     global wheel, pulsing_light, strobe, bouncing_balls, snow_sparkle, rainbow, theaterChase, theaterChaseRainbow, rainbow, orangeWipe, yellowWipe, greenWipe, blueWipe, CyanWipe, PinkWipe
 
     def orangeWipe():
-        colorWipe(strip, Color(255, 153, 51), 10)    # orange wipe
-        colorWipe(strip, Color(0, 0, 0, 0), 10)      # Composite White + White LED wipe
+        colorWipe(strip, Color(255, 153, 51), 5)    # orange wipe
+        time.sleep(900)
+        colorWipe(strip, Color(0, 0, 0, 0), 5)      # Composite White + White LED wipe
     def yellowWipe():
-        colorWipe(strip, Color(255, 255, 0), 10)     # yellow wipe
-        colorWipe(strip, Color(0, 0, 0, 0), 10)      # Composite White + White LED wipe
+        colorWipe(strip, Color(255, 255, 0), 5)     # yellow wipe
+        time.sleep(900)
+        colorWipe(strip, Color(0, 0, 0, 0), 5)      # Composite White + White LED wipe
     def greenWipe():
-        colorWipe(strip, Color(0, 255, 0), 10)       # Green wipe
-        colorWipe(strip, Color(0, 0, 0, 0), 10)      # Composite White + White LED wipe
+        colorWipe(strip, Color(0, 255, 0), 5)       # Green wipe
+        time.sleep(900)
+        colorWipe(strip, Color(0, 0, 0, 0), 5)      # Composite White + White LED wipe
     def blueWipe():
-        colorWipe(strip, Color(0, 0, 255), 10)       # Blue wipe
-        colorWipe(strip, Color(0, 0, 0, 0), 10)      # Composite White + White LED wipe    
+        colorWipe(strip, Color(0, 0, 255), 5)       # Blue wipe
+        time.sleep(900)
+        colorWipe(strip, Color(0, 0, 0, 0), 5)      # Composite White + White LED wipe    
     def CyanWipe():
-        colorWipe(strip, Color(0, 255, 255), 10)     # Cyan wipe
-        colorWipe(strip, Color(0, 0, 0, 0), 10)      # Composite White + White LED wipe
+        colorWipe(strip, Color(0, 255, 255), 5)     # Cyan wipe
+        time.sleep(900)
+        colorWipe(strip, Color(0, 0, 0, 0), 5)      # Composite White + White LED wipe
     def PinkWipe():
-        colorWipe(strip, Color(255, 0, 255), 10)     # Pink Wipe
-        colorWipe(strip, Color(0, 0, 0, 0), 10)      # Composite White + White LED wipe
+        colorWipe(strip, Color(255, 0, 255), 5)     # Pink Wipe
+        time.sleep(900)
+        colorWipe(strip, Color(0, 0, 0, 0), 5)      # Composite White + White LED wipe
 
     def wheel(pos):
         """Generate rainbow colors across 0-255 positions."""
@@ -88,61 +95,61 @@ class RGBFunctions():
             time.sleep(wait_ms/1000.0)
 
 
-    def bouncing_balls(strip,ball_count=4, wait_ms=200):
+    # def bouncing_balls(strip,ball_count=4, wait_ms=200):
 
-        start_time = time.time()
-        ClockTimeSinceLastBounce = [0 for i in range(ball_count)]
-        StartHeight=1
+    #     start_time = time.time()
+    #     ClockTimeSinceLastBounce = [0 for i in range(ball_count)]
+    #     StartHeight=1
 
-        for i in range(ball_count):
-            ClockTimeSinceLastBounce[i] = time.time()
+    #     for i in range(ball_count):
+    #         ClockTimeSinceLastBounce[i] = time.time()
     
-        Height = [0 for i in range(ball_count)]
-        Position = [0 for i in range(ball_count)]
-        ImpactVelocity = [0 for i in range(ball_count)]
-        ImpactVelocityStart= math.sqrt(-2 * -9.81 * 1)
-        Dampening = [0 for i in range(ball_count)]
-        TimeSinceLastBounce = [0 for i in range(ball_count)]
+    #     Height = [0 for i in range(ball_count)]
+    #     Position = [0 for i in range(ball_count)]
+    #     ImpactVelocity = [0 for i in range(ball_count)]
+    #     ImpactVelocityStart= math.sqrt(-2 * -9.81 * 1)
+    #     Dampening = [0 for i in range(ball_count)]
+    #     TimeSinceLastBounce = [0 for i in range(ball_count)]
 
-        for i in range(0,ball_count,1):
-            last_ClockTimeSinceLastBounce = ClockTimeSinceLastBounce[i]
-            ClockTimeSinceLastBounce[i] = time.time() - last_ClockTimeSinceLastBounce
+    #     for i in range(0,ball_count,1):
+    #         last_ClockTimeSinceLastBounce = ClockTimeSinceLastBounce[i]
+    #         ClockTimeSinceLastBounce[i] = time.time() - last_ClockTimeSinceLastBounce
 
-            Height[i] = StartHeight
-            Position[i] = 0
-            ImpactVelocity[i] = math.sqrt(-2 * -9.81 * 1)
-            TimeSinceLastBounce[i] = 0
-            Dampening[i] = 0.90 - (float(i)/(ball_count**2))
+    #         Height[i] = StartHeight
+    #         Position[i] = 0
+    #         ImpactVelocity[i] = math.sqrt(-2 * -9.81 * 1)
+    #         TimeSinceLastBounce[i] = 0
+    #         Dampening[i] = 0.90 - (float(i)/(ball_count**2))
 
-        while True:
-            for i in range(ball_count):
-                TimeSinceLastBounce[i] = time.time() - ClockTimeSinceLastBounce[i]
-                Height[i] = 0.5 * (-9.81) * (TimeSinceLastBounce[i]**2) + ImpactVelocity[i] * TimeSinceLastBounce[i]
-                if (Height[i] < 0):
-                    Height[i] = 0
-                    ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i]
-                    ClockTimeSinceLastBounce[i] = time.time()
-                    if (ImpactVelocity[i] < 0.01):
-                        ImpactVelocity[i] = ImpactVelocityStart
+    #     while True:
+    #         for i in range(ball_count):
+    #             TimeSinceLastBounce[i] = time.time() - ClockTimeSinceLastBounce[i]
+    #             Height[i] = 0.5 * (-9.81) * (TimeSinceLastBounce[i]**2) + ImpactVelocity[i] * TimeSinceLastBounce[i]
+    #             if (Height[i] < 0):
+    #                 Height[i] = 0
+    #                 ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i]
+    #                 ClockTimeSinceLastBounce[i] = time.time()
+    #                 if (ImpactVelocity[i] < 0.01):
+    #                     ImpactVelocity[i] = ImpactVelocityStart
                                   
-                Position[i] = round(Height[i] * (strip.numPixels()-1)/StartHeight)   #Hier wird die relative Höhe auf die absolute Höhe mit der LED Anzahl umgewandelt.
-            for i in range(ball_count):
-                strip.setPixelColorRGB(Position[i], 0, 0,255)    
-            strip.show()
-            for i in range(strip.numPixels()):
-                strip.setPixelColorRGB(i, 0,0,0)
+    #             Position[i] = round(Height[i] * (strip.numPixels()-1)/StartHeight)   #Hier wird die relative Höhe auf die absolute Höhe mit der LED Anzahl umgewandelt.
+    #         for i in range(ball_count):
+    #             strip.setPixelColorRGB(Position[i], 0, 0,255)    
+    #         strip.show()
+    #         for i in range(strip.numPixels()):
+    #             strip.setPixelColorRGB(i, 0,0,0)
             
-    def snow_sparkle(strip,sparkle_delay=20):
-        from random import randint
-        pixel= randint(0,strip.numPixels())
-        speed_delay=randint(100,1000)
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, Color(0x10,0x10,0x10))
-        strip.show()
-        time.sleep(speed_delay/1000.0)
-        strip.setPixelColorRGB(pixel, 255,255,255)
-        strip.show()
-        time.sleep(sparkle_delay/1000.0)
+    # def snow_sparkle(strip,sparkle_delay=20):
+    #     from random import randint
+    #     pixel= randint(0,strip.numPixels())
+    #     speed_delay=randint(100,1000)
+    #     for i in range(strip.numPixels()):
+    #         strip.setPixelColor(i, Color(0x10,0x10,0x10))
+    #     strip.show()
+    #     time.sleep(speed_delay/1000.0)
+    #     strip.setPixelColorRGB(pixel, 255,255,255)
+    #     strip.show()
+    #     time.sleep(sparkle_delay/1000.0)
     
     
    
@@ -226,42 +233,21 @@ if __name__ == '__main__':
 
         while True:
             
-            for i in range(4):
-                #print(i)
-                orangeWipe()                            #Orange Wipe for Loop
-                time.sleep(1)
-            if i == 3:
-                i == 0 
-                for i in range(4):
-                    #print(i)
-                    blueWipe()                          #Blue Wipe for Loop
-                    time.sleep(1)
-                if i == 3:
-                    i == 0
-                    for i in range(4):
-                        yellowWipe()                    #Yellow Wipe for Loop
-                        time.sleep(1)
-                    if i == 3:
-                        i == 0
-                        for i in range(4):
-                            #print(i)
-                            greenWipe()                 #Green Wipe for Loop
-                            time.sleep(1)
-                        if i == 3:
-                            i == 0
-                            for i in range(4):
-                                #print(i)
-                                CyanWipe()              #Cyan Wipe for Loop
-                                time.sleep(1)
-                            if i == 3:
-                                i == 0
-                                for i in range(4):
-                                    #print(i)
-                                    PinkWipe()          #Pink Wipe for Loop
-                                    time.sleep(1)        
+            orangeWipe()
+            time.sleep(1)
+            yellowWipe()
+            time.sleep(1)
+            greenWipe()
+            time.sleep(1)
+            CyanWipe()
+            time.sleep(1)
+            blueWipe()
+            time.sleep(1)
+            PinkWipe()
+            time.sleep(1)        
     except KeyboardInterrupt:
         if args.clear:
             terminateSequence()
 
     except SystemExit:
-        terminateSequence()
+        terminateSequence() 
--- v.environment/test.py
+++ v.environment/test.py
@@ -1,5 +1,6 @@
 from ast import Try
 from distutils.command.clean import clean
+from itertools import cycle
 from pickle import GLOBAL
 import time 
 i = 0
@@ -48,44 +49,67 @@ def greenWipe():
 #<---------------------------------------------------------->
 
 try:
-
     while True:
-
-        for i in range(4):
-            #print(i)
-            orangeWipe()                            #Orange Wipe for Loop
-            time.sleep(1)
-        if i == 3:
-            i == 0 
-            for i in range(4):
-                #print(i)
-                blueWipe()                          #Blue Wipe for Loop
-                time.sleep(1)
-            if i == 3:
-                i == 0
-                for i in range(4):
-                    yellowWipe()                    #Yellow Wipe for Loop
-                    time.sleep(1)
-                if i == 3:
-                    i == 0
-                    for i in range(4):
-                        #print(i)
-                        greenWipe()                 #Green Wipe for Loop
-                        time.sleep(1)
-                    if i == 3:
-                        i == 0
-                        for i in range(4):
-                            #print(i)
-                            CyanWipe()              #Cyan Wipe for Loop
-                            time.sleep(1)
-                        if i == 3:
-                            i == 0
-                            for i in range(4):
-                                #print(i)
-                                PinkWipe()          #Pink Wipe for Loop
-                                time.sleep(1)    
+        orangeWipe()
+        time.sleep(900)
+        yellowWipe()
+        time.sleep(900)
+        greenWipe()
+        time.sleep(900)
+        CyanWipe()
+        time.sleep(900)
+        blueWipe()
+        time.sleep(900)
+        PinkWipe()
+        time.sleep(900)
 except KeyboardInterrupt:
     clean
+
+
+
+
+
+
+
+# try:
+
+#     while True:
+
+#         for i in range(4):
+#             #print(i)
+#             orangeWipe()                            #Orange Wipe for Loop
+#             time.sleep(1)
+#         if i == 3:
+#             i == 0 
+#             for i in range(4):
+#                 #print(i)
+#                 blueWipe()                          #Blue Wipe for Loop
+#                 time.sleep(1)
+#             if i == 3:
+#                 i == 0
+#                 for i in range(4):
+#                     yellowWipe()                    #Yellow Wipe for Loop
+#                     time.sleep(1)
+#                 if i == 3:
+#                     i == 0
+#                     for i in range(4):
+#                         #print(i)
+#                         greenWipe()                 #Green Wipe for Loop
+#                         time.sleep(1)
+#                     if i == 3:
+#                         i == 0
+#                         for i in range(4):
+#                             #print(i)
+#                             CyanWipe()              #Cyan Wipe for Loop
+#                             time.sleep(1)
+#                         if i == 3:
+#                             i == 0
+#                             for i in range(4):
+#                                 #print(i)
+#                                 PinkWipe()          #Pink Wipe for Loop
+#                                 time.sleep(1)    
+# except KeyboardInterrupt:
+#     clean
 
 
 
